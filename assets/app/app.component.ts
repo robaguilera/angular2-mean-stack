1a5f19513148356634363347eadfab0216b26ae7
Robert Aguilera <raguilera@deciperinc.com>
2017-08-10 12:04:33 -0700

    start of angular 2 components

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -5,5 +5,8 @@ import { Component } from '@angular/core';
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-    
+    message = {
+        content: 'A Message',
+        author: 'Robert'
+    }
 }
